65966e4fdf0e0a26fc52f5cb36e41c351dca7213
Warren Gay <ve3wwg@gmail.com>
2020-04-26 10:21:15 -0400

    Improved to eliminate reply queue

--- gatekeeper/gatekeeper.ino
+++ gatekeeper/gatekeeper.ino
@@ -34,15 +34,12 @@
 #define GATEKRDY      0b0001    // Gatekeeper ready
 #define STOP          int(1)    // Arduino I2C API
 
-// (*) initialized by setup()
-
 static struct s_gatekeeper {
   EventGroupHandle_t  grpevt;   // Group Event handle(*)
   QueueHandle_t       queue;    // Request queue handle
-  QueueHandle_t       replyq;   // Reply queue handle
   uint8_t             states[N_DEV]; // Current states
 } gatekeeper = {
-  nullptr, nullptr, nullptr, { 0xFF, 0xFF }
+  nullptr, nullptr, { 0xFF, 0xFF }
 };
 
 // Message struct for Message/Response queue
@@ -51,6 +48,7 @@ struct s_ioport {
   uint8_t value : 1;  // Bit value
   uint8_t error : 1;  // Error bit, when used
   uint8_t port : 5;   // Port number
+  TaskHandle_t htask; // Reply Task handle
 };
 
 // Gatekeeper task: Owns I2C bus operations and
@@ -60,15 +58,13 @@ static void gatekeeper_task(void *arg) {
   static int i2caddr[N_DEV] = { DEV0, DEV1 };
   int addr;             // Device I2C address
   uint8_t devx, portx;  // Device index, bit index
-  s_ioport ioport;      // Queue message
-  uint8_t data;         // Data byte
+  s_ioport *pioport;    // Queue message pointer
+  uint8_t data;         // Temp data byte
   BaseType_t rc;        // Return code
 
   // Create API communication queues
-  gatekeeper.queue = xQueueCreate(8,sizeof(s_ioport));
+  gatekeeper.queue = xQueueCreate(8,sizeof(s_ioport*));
   assert(gatekeeper.queue);
-  gatekeeper.replyq = xQueueCreate(8,sizeof(s_ioport));
-  assert(gatekeeper.replyq);
 
   // Start I2C Bus Support:
   Wire.begin(I2C_SDA,I2C_SCL);
@@ -83,59 +79,57 @@ static void gatekeeper_task(void *arg) {
     assert(!rc); // I2C Fail?
   }
 
-  // Mark gatekeeper as ready for use:
+  // Indicate gatekeeper ready for use:
   xEventGroupSetBits(gatekeeper.grpevt,GATEKRDY);
   
-  // Gatekeeper Event loop
+  // Event loop
   for (;;) {
     // Receive command:
-    rc = xQueueReceive(gatekeeper.queue,&ioport,
+    rc = xQueueReceive(gatekeeper.queue,&pioport,
       portMAX_DELAY);
     assert(rc == pdPASS);
 
-    devx = ioport.port / 8;   // device index
-    portx = ioport.port % 8;  // pin index
+    devx = pioport->port / 8;   // device index
+    portx = pioport->port % 8;  // pin index
     assert(devx < N_DEV);
-    addr = i2caddr[devx];     // device address
+    addr = i2caddr[devx];       // device address
 
-    if ( ioport.input ) {
+    if ( pioport->input ) {
       // COMMAND: READ A GPIO BIT:
       Wire.requestFrom(addr,1,STOP);
       rc = Wire.available();
       if ( rc > 0 ) {
-        data = Wire.read();   // Read all bits
-        ioport.error = false; // Successful
-        ioport.value = !!(data & (1 << portx));
+        data = Wire.read();     // Read all bits
+        pioport->error = false; // Successful
+        pioport->value = !!(data & (1 << portx));
       } else {
         // Return GPIO fail:
-        ioport.value = false;
-        ioport.error = true;
+        pioport->error = true;
+        pioport->value = false;
       }
     } else {
       // COMMAND: WRITE A GPIO BIT:
       data = gatekeeper.states[devx];
-      if ( ioport.value )
+      if ( pioport->value )
         data |= 1 << portx;   // Set a bit
       else
         data &= ~(1 << portx); // Clear a bit
       Wire.beginTransmission(addr);
       Wire.write(data);
-      ioport.error = Wire.endTransmission() != 0;
-      if ( !ioport.error )
+      pioport->error = Wire.endTransmission() != 0;
+      if ( !pioport->error )
         gatekeeper.states[devx] = data;
     }    
 
-    // Reply to client:
-    rc = xQueueSendToBack(
-      gatekeeper.replyq,
-      &ioport,
-      portMAX_DELAY);
-    assert(rc == pdPASS);
+    // Notify client about completion
+    assert(pioport->htask);
+    xTaskNotifyGive(pioport->htask);
   }
 }
 
 // Block caller until gatekeeper ready:
 static void pcf8574_wait_ready() {
+
   xEventGroupWaitBits(
     gatekeeper.grpevt, // Event group handle
     GATEKRDY,     // Bits to wait for
@@ -153,24 +147,24 @@ static void pcf8574_wait_ready() {
 
 static short pcf8574_get(uint8_t port) {
   s_ioport ioport;      // Port pin (0..15)
+  s_ioport *pioport = &ioport;
   BaseType_t rc;
 
   assert(port < 16);
   ioport.input = true;  // Read request
   ioport.port = port;   // 0..15 port pin
+  ioport.htask = xTaskGetCurrentTaskHandle();
+
   pcf8574_wait_ready(); // Block until ready
 
   rc = xQueueSendToBack(
     gatekeeper.queue,
-    &ioport,
+    &pioport,
     portMAX_DELAY);
   assert(rc == pdPASS);  
   
-  rc = xQueueReceive( 
-    gatekeeper.replyq,
-    &ioport,
-    portMAX_DELAY);
-  assert(rc == pdPASS);
+  // Wait to be notified:
+  ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
   return ioport.error ? -1 : ioport.value;
 }
 
@@ -178,37 +172,43 @@ static short pcf8574_get(uint8_t port) {
 // RETURNS:
 //  0 or 1: Succesful bit write
 //  -1:     Failed GPIO write
+
 static short pcf8574_put(uint8_t port,bool value) {
   s_ioport ioport;      // Port pin (0..15)
+  s_ioport *pioport = &ioport;
   BaseType_t rc;
 
   assert(port < 16);
   ioport.input = false; // Write request
   ioport.value = value; // Bit value
   ioport.port = port;   // PCF8574 port pin
+  ioport.htask = xTaskGetCurrentTaskHandle();
+
   pcf8574_wait_ready(); // Block until ready
 
   rc = xQueueSendToBack(
     gatekeeper.queue,
-    &ioport,
+    &pioport,
     portMAX_DELAY);
   assert(rc == pdPASS);  
   
-  rc = xQueueReceive(
-    gatekeeper.replyq,
-    &ioport,
-    portMAX_DELAY);
-  assert(rc == pdPASS);
+  // Wait to be notified:
+  ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
   return ioport.error ? -1 : ioport.value;
 }
 
 // User task: Uses gatekeeper task for
-// reading/setting PCF8574 port pins.
-// Pins range: 0..15
+// reading/writing PCF8574 port pins.
+//
+// Pins: 
+//  0..7    Device 0 (address DEV0)
+//  8..15   Device 1 (address DEV1)
 //
 // Detect button press, and then activate
 // corresponding LED.
+
 static void usr_task(void *argp) {
+  QueueHandle_t replyq;   // Reply queue handle
   struct s_state {
     uint8_t   button;
     uint8_t   led;
@@ -236,11 +236,15 @@ static void usr_task(void *argp) {
   }
 }
 
+// Initialize Application
+
 void setup() {
   int app_cpu = xPortGetCoreID();
   BaseType_t rc;  // Return code
 
-  // Create Event Group for Gatekeeper
+  // Create Event Group for Gatekeeper.
+  // This must be created before any using
+  // tasks execute.
   gatekeeper.grpevt = xEventGroupCreate();
   assert(gatekeeper.grpevt);
 
@@ -249,6 +253,7 @@ void setup() {
   printf("\ngatekeeper.ino:\n");
 #endif
 
+  // Start the gatekeeper task
   rc = xTaskCreatePinnedToCore(
     gatekeeper_task,
     "gatekeeper", // Name
@@ -260,6 +265,7 @@ void setup() {
   );
   assert(rc == pdPASS);
 
+  // Start user task 1
   rc = xTaskCreatePinnedToCore(
     usr_task,   // Function
     "usr_task", // Name
